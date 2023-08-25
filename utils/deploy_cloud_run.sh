028fa1fe861db8de757cf578e587472300a43aa2
GabsLeo <gabrieldemoraistaveira@gmail.com>
2023-08-25 09:40:26 -0300

    Update deploy_cloud_run.sh

--- utils/deploy_cloud_run.sh
+++ utils/deploy_cloud_run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+gcloud config set account purplerelay-github
 for region in $5
 do
     gcloud run deploy $1-$region-app-$2 --project $1 --region $region --image us-east1-docker.pkg.dev/$1/$3/strfry:$4 --update-env-vars BUCKET=$1-$region-bucket-$2

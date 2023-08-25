8a97f573b7752178cda4247a0dbf7ae96191dea6
GabsLeo <gabrieldemoraistaveira@gmail.com>
2023-08-25 09:54:57 -0300

    Update deploy_cloud_run.sh

--- utils/deploy_cloud_run.sh
+++ utils/deploy_cloud_run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cp -r ./application_default_credentials.json $HOME/.config/gcloud/application_default_credentials.json
 gcloud config set account "Compute Engine default service account"
-gcloud auth activate-service-account "Compute Engine default service account"
+gcloud auth activate-service-account "Compute Engine default service account" --key-file="application_default_credentials.json"
 for region in $5
 do
     gcloud run deploy $1-$region-app-$2 --project $1 --region $region --image us-east1-docker.pkg.dev/$1/$3/strfry:$4 --update-env-vars BUCKET=$1-$region-bucket-$2

ccf540c2bf7d6873e269b8b369f21ed5b56182fc
GabsLeo <gabrieldemoraistaveira@gmail.com>
2023-08-25 12:09:24 -0300

    Update generate_array.sh

--- utils/generate_array.sh
+++ utils/generate_array.sh
@@ -2,4 +2,4 @@
 sudo apt install jq
 X=($@)
 export GENERATED_ARRAY=$(jq --compact-output --null-input '$ARGS.positional' --args -- ""${X[@]}"")
-echo $GENERATED_ARRAY
+echo $GENERATED_ARRAY > regions.txt

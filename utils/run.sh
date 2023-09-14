588d7b739fd79213e29684b25771c6486558a0d8
gesse_carlos <gesse.cassa@tmaior.com.br>
2023-09-14 15:52:28 -0300

    Test: deploy without cloud fuse

--- utils/run.sh
+++ utils/run.sh
@@ -6,10 +6,11 @@ mkdir -p $MNT_DIR
 
 cp -r ./nginx.conf /etc/nginx/nginx.conf
 cp -r ./new.default.conf /etc/nginx/sites-enabled/default.conf
+cat /etc/nginx/sites-enabled/default.conf
 
-echo "Mounting GCS Fuse."
-gcsfuse --debug_gcs --debug_fuse $BUCKET $MNT_DIR
-echo "Mounting completed."
+# echo "Mounting GCS Fuse."
+# gcsfuse --debug_gcs --debug_fuse $BUCKET $MNT_DIR
+# echo "Mounting completed."
 
 # Start the application
 nginx

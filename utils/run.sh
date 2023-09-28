cc270425ce9e74763a38d473f678feb3d21e0987
gesse_carlos <gesse.cassa@tmaior.com.br>
2023-09-28 05:40:28 -0300

    Fix: nginx www-data user fix

--- utils/run.sh
+++ utils/run.sh
@@ -1,24 +1,22 @@
 #!/usr/bin/env sh
 set -eo pipefail
 
-# mkdir /etc/nginx/sites-available
-# mkdir /etc/nginx/sites-enabled
+mkdir -p /etc/nginx/sites-enabled
+mkdir -p /etc/nginx/sites-available
 
-# cp -r ./nginx.conf /etc/nginx/nginx.conf
-# cp -r ./new.default.conf /etc/nginx/sites-enabled/default.conf
+cp -r ./nginx.conf /etc/nginx/nginx.conf
+cp -r ./new.default.conf /etc/nginx/sites-enabled/default.conf
 
-# ln -s /etc/nginx/sites-available/default.conf /etc/nginx/sites-enabled
-
-go install github.com/googlecloudplatform/gcsfuse@latest
+ln -s /etc/nginx/sites-enabled/default /etc/nginx/sites-available/default
 
 echo "Mounting GCS Fuse."
 /go/bin/gcsfuse --debug_gcs --debug_fuse $BUCKET $MNT_DIR
 echo "Mounting completed."
 
-# adduser -s /bin/false nginx
+adduser -S www-data
 
 # Start the application
-# nginx
+nginx
 ./strfry relay
 # Exit immediately when one of the background processes terminate.
 wait -n

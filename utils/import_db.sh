26a80440f99216c208fa00ba9ce08c36b017a6de
gesse_carlos <gesse.cassa@tmaior.com.br>
2023-09-29 07:28:13 -0300

    fix: home path

--- utils/import_db.sh
+++ utils/import_db.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 set -eo pipefail
 
-/google-cloud-sdk/bin/gcloud auth activate-service-account --key-file ./$HOME/.config/gcloud/application_default_credentials.json
+/google-cloud-sdk/bin/gcloud auth activate-service-account --key-file $HOME/.config/gcloud/application_default_credentials.json
 
 /google-cloud-sdk/bin/gsutil cp gs://purple_caddy/purplerelay.com/dump.jsonl.zst ./dump.json.zst
 

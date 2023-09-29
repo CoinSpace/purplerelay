60318f552ef0d204ed11cb7fe2d8779a8735173b
gesse_carlos <gesse.cassa@tmaior.com.br>
2023-09-29 06:42:48 -0300

    update gsutil path

--- utils/import_db.sh
+++ utils/import_db.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 set -eo pipefail
 
-export PATH=/google-cloud-sdk/bin:${PATH}
+/google-cloud-sdk/bin/gsutil cp gs://purple_caddy/purplerelay.com/dump.jsonl.zst ./dump.json.zst
 
-gsutil cp gs://purple_caddy/purplerelay.com/dump.jsonl.zst ./dump.json.zst
+./strfry import < ./dump.jsonl.zst

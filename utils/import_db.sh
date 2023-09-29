9e2c94512a5848fb8f4bbad14bc9d6867d3b4261
gesse_carlos <gesse.cassa@tmaior.com.br>
2023-09-29 07:18:14 -0300

    Test: gcloud auth

--- utils/import_db.sh
+++ utils/import_db.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env sh
 set -eo pipefail
 
+/google-cloud-sdk/bin/gcloud auth activate-service-account --key-file ./$HOME/.config/gcloud/application_default_credentials.json
+
 /google-cloud-sdk/bin/gsutil cp gs://purple_caddy/purplerelay.com/dump.jsonl.zst ./dump.json.zst
 
 ./strfry import < ./dump.jsonl.zst

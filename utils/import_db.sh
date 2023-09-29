a56f34674662bc72a8ac655d99a2b5f17a5b492b
gesse_carlos <gesse.cassa@tmaior.com.br>
2023-09-29 08:02:20 -0300

    Test: move dump.json to workdir

--- utils/import_db.sh
+++ utils/import_db.sh
@@ -3,8 +3,8 @@ set -eo pipefail
 
 /google-cloud-sdk/bin/gcloud auth activate-service-account --key-file /root/.config/gcloud/application_default_credentials.json
 
-/google-cloud-sdk/bin/gsutil cp gs://purple_caddy/purplerelay.com/dump.jsonl.zst ./dump.json.zst
+/google-cloud-sdk/bin/gsutil cp gs://purple_caddy/purplerelay.com/dump.jsonl.zst /app/dump.json.zst
 
 ls -lhtra
 
-./strfry import < ./dump.jsonl.zst
+./strfry import < /app/dump.json.zst

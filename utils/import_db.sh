8d33018fb8874ee5547dc99529fa27cbc3e97934
gesse_carlos <gesse.cassa@tmaior.com.br>
2023-10-27 17:06:15 -0300

    Test: Change the location of db dump

--- utils/import_db.sh
+++ utils/import_db.sh
@@ -3,9 +3,6 @@ set -eo pipefail
 
 /google-cloud-sdk/bin/gcloud auth activate-service-account --key-file /root/.config/gcloud/application_default_credentials.json
 
-/google-cloud-sdk/bin/gsutil cp gs://purple_caddy/purplerelay.com/dump.jsonl.zst /app/dump.jsonl.zst
+/google-cloud-sdk/bin/gsutil cp gs://purple_caddy/purplerelay.com/dump.jsonl.zst /app/strfry-db/dump.jsonl.zst
 
-zstd -d /app/dump.jsonl.zst
-
-
-rm /app/dump.jsonl.zst
+zstd -d /app/strfry-db/dump.jsonl.zst

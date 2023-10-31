5aea0d2a70ce112826eb8011a0f465293fb731df
gesse_carlos <gesse.cassa@tmaior.com.br>
2023-10-31 04:22:25 -0300

    Test: import and run relay without wait -n command

--- utils/run.sh
+++ utils/run.sh
@@ -15,12 +15,7 @@ echo "Mounting completed."
 
 adduser -S www-data
 
-./strfry import < /app/dump.jsonl --no-verify
-wait -n
-
 nginx
 ./strfry relay
-wait -n
 
-# ./strfry import < /app/dump.jsonl --no-verify
-# wait -n
+./strfry import < /app/dump.jsonl --no-verify

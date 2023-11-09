d4f5e0f2fdcb7b52c89f617ff39b71788269ab62
gesse_carlos <gesse.cassa@tmaior.com.br>
2023-11-09 00:59:35 -0300

    Feat: dockerfile entrypoint script

---
+++ aws/scripts/entrypoint.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+set -eo pipefail
+
+mkdir -p /etc/nginx/sites-enabled
+mkdir -p /etc/nginx/sites-available
+
+cp -r ./nginx.conf /etc/nginx/nginx.conf
+cp -r ./default.conf /etc/nginx/sites-enabled/default.conf
+
+ln -s /etc/nginx/sites-enabled/default /etc/nginx/sites-available/default
+
+adduser -S www-data
+
+nginx
+./strfry relay
+./strfry import < ./dump.jsonl --no-verify &

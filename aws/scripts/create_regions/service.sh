df6a6038b7c2701e4f24f4a53d533c3fe7c2d9d8
gesse_carlos <gesse.cassa@tmaior.com.br>
2024-02-15 16:40:58 -0300

    Feat: Script to create cloudformation service stacks

---
+++ aws/scripts/create_regions/service.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+    echo "Error: Please provide the REGION as the first argument."
+    exit 1
+fi
+REGION="$1"
+
+if [ -z "$2" ]; then
+    echo "Error: Please provide the codestar-connection arn as the second argument."
+    exit 1
+fi
+CONNECTION_ARN="$2"
+
+if [ -z "$3" ]; then
+    echo "Error: Please provide the Certificate-arn as the third argument."
+    exit 1
+fi
+CERTIFICATE_ARN="$3"
+
+aws s3 rm s3://service-artifact-"$REGION" --recursive || \
+  echo "The S3 bucket content doesn't exist or it's already deleted. Skipping deletion."
+
+
+aws s3 rb s3://service-artifact-"$REGION" || \
+  echo "The S3 bucket doesn't exist or it's already deleted. Skipping deletion."
+
+
+aws iam delete-role-policy \
+  --role-name cfn-service-pipeline-"$REGION" \
+  --policy-name root || \
+  echo "The role policy doesn't exist or it's already deleted. Skipping deletion."
+
+
+aws iam delete-role \
+  --role-name cfn-service-pipeline-"$REGION" || \
+  echo "The role doesn't exist or it's already deleted. Skipping deletion."
+
+
+aws ecr delete-repository \
+  --repository-name purplerelay \
+  --region $REGION \
+  --force || \
+  echo "The repository doesn't exist or it's already deleted. Skipping deletion."
+
+aws cloudformation create-stack \
+  --stack-name service-pipeline \
+  --template-body file://../../cloudformation/service/codepipeline.yml \
+  --parameters \
+    ParameterKey=ConnectionArn,ParameterValue="$CONNECTION_ARN" \
+    ParameterKey=DefaultAcmCertificateArn,ParameterValue="$CERTIFICATE_ARN" \
+  --capabilities CAPABILITY_NAMED_IAM \
+  --region "$REGION"

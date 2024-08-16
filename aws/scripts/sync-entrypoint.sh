f9c37dfbeaa9bb176ad5abf0e49c868077b2f085
tmaior <tlmaior@gmail.com>
2024-08-16 19:11:35 -0300

    update router entrypoint

--- aws/scripts/sync-entrypoint.sh
+++ aws/scripts/sync-entrypoint.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env sh
 # set -eo pipefail
+set -x
 
 mkdir -p /etc/nginx/sites-enabled
 mkdir -p /etc/nginx/sites-available
@@ -7,10 +8,10 @@ mkdir -p /etc/nginx/sites-available
 cp -r ./nginx.conf /etc/nginx/nginx.conf
 cp -r ./default.conf /etc/nginx/sites-enabled/default.conf
 
-ln -s /etc/nginx/sites-enabled/default /etc/nginx/sites-available/default
+ln -s /etc/nginx/sites-enabled/default /etc/nginx/sites-available/default || echo "Link to default nginx conf already exists"
 
 adduser -S www-data
 
 nginx
 ./strfry sync wss://purplerelay.com --dir both
-./strfry router /etc/strfry-router.config &
+./strfry router /etc/strfry-router.config
